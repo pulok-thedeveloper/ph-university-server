8f816b74d64b431315b89fc39ebd87d53a7232b9
pulok-thedeveloper <mdpulokhasan@gmail.com>
2024-08-09 02:04:34 +0600

    project setup

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors"
+
+const app: Application = express();
+const port = 5000;
+
+//parsers
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world");
+});
+
+export default app;
